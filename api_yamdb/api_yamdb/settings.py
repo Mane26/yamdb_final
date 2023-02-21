bf3491b6f14bc67a8255e29fb6f3652a9a1a37d2
Meline Sarkisyan <s.m.m21-08-90@mail.ru>
2023-02-21 23:26:30 +0300

    2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your_secret_key')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '84.252.141.70',
+    '158.160.24.121',
     'localhost',
     '127.0.0.1',
     'web',
